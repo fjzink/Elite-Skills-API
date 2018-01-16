38b951c637c760de1ed56a1625771c4e76a5b4e4
Fulton Zink <joeyzink@gmail.com>
2018-01-16 15:25:28 -0800

    Add seeds for user model. Add has_secure_ password to user model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+ß    @user = User.new(user_params)
 
     if @user.save
       render json: @user, status: :created, location: @user
