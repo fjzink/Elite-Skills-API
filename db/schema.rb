ecd6d0f90141673d05f2df7f7c901684cdfe625b
Fulton Zink <joeyzink@gmail.com>
2018-01-18 12:10:07 -0800

    Add activerecord relations for group resource. Add group seeds to seed file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116214630) do
+ActiveRecord::Schema.define(version: 20180118011740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
-5.times do
+User.delete_all
+Group.delete_all
+
+5.times do |id|
   User.create(username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password")
+
+  Group.create(group: Faker::Job.field, description: Faker::Lorem.sentence, user_id: id+1)
 end
